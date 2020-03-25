2188ad6ef1e18056fde23b02576be94ae86574d2
Yzhaoyiming <1239069678@qq.com>
2020-03-25 14:25:18 +0800

    new

--- page/main1.js
+++ page/main1.js
@@ -32,6 +32,32 @@ $(function() {
         '变化幅度',
       ]
     });
-  
-    return hot;
+    var myChart = echarts.init(document.getElementById('mainx'));
+
+    // 指定图表的配置项和数据
+    var option = {
+        title: {
+            text: 'JavaScript语言排名变化'
+        },
+        tooltip: {
+            trigger : 'axis',
+    axisPointer : {
+      type : 'line',
+    }
+
+        },  
+        xAxis: {
+            data: ["2000","2005","2010","2015","2020"]
+        },
+        yAxis: {},
+        series: [{
+            name: '排名',
+            type: 'line',
+            data: [6, 9, 8, 8, 7]
+        }]
+    };
+
+    // 使用刚指定的配置项和数据显示图表。
+    myChart.setOption(option);
+    // return hot;
   });
